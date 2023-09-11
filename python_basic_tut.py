be499bcf4cccdb1a2175e24e249d3ced1c8ef150
A-Meda23 <your email>
2023-09-11 19:12:07 -0400

    11th commit -python syntax comment added

--- python_basic_tut.py
+++ python_basic_tut.py
@@ -1,2 +1,14 @@
 # 10th commit -print hello world
 print("hello world")
+
+#Python Indentation
+##Indentation refers to the spaces at the beginning of a code line.
+##Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
+##Python uses indentation to indicate a block of code.
+if 5 > 2:
+  print("Five is greater than two!")
+
+#Python Variables: In Python, variables are created when you assign a value to it:
+x = 5
+y = "Hello, World!"
+print(x, y)
